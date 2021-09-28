ba492278824eef8f16baa9c0de4b0a2568ee8d99
Charlie Weinberger <70112746+charliebrown364@users.noreply.github.com>
2021-09-28 22:49:18 +0000

    finished tests

--- test.py
+++ test.py
@@ -9,20 +9,22 @@ from wait_strat import *
 # test number 1
 players = [Player(SameMove(move=(0, -1))), Player(SameMove(move=(0, -1)))]
 game = Game(players, show_game=False)
-game.run_if(lambda x: game.turn <= 3)
-old_coords = players[0].ships[0].coords
-game.run_if(lambda x: game.turn <= 4)
+initial_coords = players[0].ships[0].coords
+game.run_if(lambda x: x.turn <= 1)
 new_coords = players[0].ships[0].coords
 
-assert old_coords == new_coords
+assert initial_coords == new_coords
 
 # test number 2
 players = [Player(MoveToOpponent()), Player(Wait())]
 game = Game(players, show_game=False)
-game.run_if(lambda x: game.turn <= 5)
-game.complete_movement_phase()
+game.run_if()
 
 assert game.board[game.combat_coords[0]][0].player_num == 2
 
 # test number 3
-# look at game.check_for_winner()
+players = [Player(SameMove(move=(1, 0))), Player(SameMove(move=(0, -1)))]
+game = Game(players, show_game=False)
+game.run_if(lambda x: x.turn <= 6)
+
+assert game.winner == 2
