20cc76e0f72c9acac4bc1ad35b0ddf4b267869ab
Charlie Weinberger <364charliebrown@gmail.com>
2021-11-23 10:03:26 +0000

    added new ship types

--- ships.py
+++ ships.py
@@ -6,19 +6,6 @@ class Ship:
     def ship_id(self):
         return f'{self.player_num} {self.name} {self.ship_num}'
 
-'''
-
-{
-    'Battleship': {'cp_cost': 20, 'attack': 5, 'defense': 2},
-    'Battlecruiser': {'cp_cost': 15, 'attack': 5, 'defense': 1},
-    'Cruiser': {'cp_cost': 12, 'attack': 4, 'defense': 1},
-    'Destroyer': {'cp_cost': 9, 'attack': 4, 'defense': 0},
-    'Dreadnaught': {'cp_cost': 24, 'attack': 6, 'defense': 3},
-    'Scout': {'cp_cost': 6, 'attack': 3, 'defense': 0},
-}
-
-'''
-
 class Scout(Ship):
     
     def __init__(self, player_num, ship_num, initial_coords):
@@ -32,6 +19,7 @@ class Scout(Ship):
         self.atk = 3
         self.df = 0
         self.hp = 1
+        self.cp_cost = 6
 
 class BattleCruiser(Ship):
     
@@ -46,3 +34,49 @@ class BattleCruiser(Ship):
         self.atk = 5
         self.df = 1
         self.hp = 2
+        self.cp_cost = 15
+
+class Cruiser(Ship):
+
+    def __init__(self, player_num, ship_num, initial_coords):
+
+        self.player_num = player_num
+        self.ship_num = ship_num
+        self.coords = init_coords
+
+        self.name = "Cruiser"
+        self.ship_class = "C"
+        self.atk = 4
+        self.df = 1
+        self.hp = 2
+        self.cp_cost = 12
+
+class Destroyer(Ship):
+    
+    def __init__(self, player_num, ship_num, initial_coords):
+        
+        self.player_num = player_num
+        self.ship_num = ship_num
+        self.coords = init_coords
+
+        self.name = "Destroyer"
+        self.ship_class = "D"
+        self.atk = 4
+        self.df = 0
+        self.hp = 1
+        self.cp_cost = 9
+
+class Dreadnaught(Ship):
+    
+    def __init__(self, player_num, ship_num, initial_coords):
+
+        self.player_num = player_num
+        self.ship_num = ship_num
+        self.coords = initial_coords
+
+        self.name = "Dreadnaught"
+        self.ship_class = "A"
+        self.atk = 6
+        self.df = 3
+        self.hp = 3
+        self.cp_cost = 24
